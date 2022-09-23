0efb7a8523b2d4d33d947b5bca1d82680af91809
Ivan Amoshyi <amoshyiivan@gmail.com>
2022-09-23 13:31:09 +0300

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,64 +1,21 @@
 import '../css/style.css';
-import imageTpl from '../templates/images.hbs';
 
+let slides = document.getElementsByClassName("slider__slide");
+let currentSlide = 0;
 
+document.getElementById("nav-button--next").addEventListener("click", () => {
+    changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+    changeSlide(currentSlide - 1)
+});
 
-const imageContainerEl = document.querySelector('.images-list');
-const buttonEl = document.querySelector('.start-button');
-buttonEl.addEventListener('click', fetchImages)
+function changeSlide(moveTo) {
+    if (moveTo >= slides.length) {moveTo = 0;}
+    if (moveTo < 0) {moveTo = slides.length - 1;}
+    
+    slides[currentSlide].classList.toggle("active");
+    slides[moveTo].classList.toggle("active");
 
-function fetchImages() {
-  fetch('https://pixabay.com/api/?key=29734383-6ec437d7a0c5df52cef54a0f9&q=tree&image_type=photo&per_page=10')
-    .then(r => r.json())
-    .then(r => {
-      renderImage(r.hits)
-    })
-    .catch(error => console.log(error));
+    currentSlide = moveTo;
 }
-
-function renderImage(image) {
-  const markup = imageTpl(image);
-  imageContainerEl.innerHTML = markup;
-}
-
-
-// const drivers = [
-//   'Alfa Romeo',
-//   'Williams',
-//   'Red Bull',
-//   'Mercedes',
-//   'AlphaTauri',
-//   'Alpine',
-//   'McLaren',
-//   'Haas',
-//   'Ferrari',
-//   'Aston Martin'
-// ]
-
-
-// const buttonEl = document.querySelector('.start-button');
-// const winnerEl = document.querySelector('.winner');
-// // const resultsEl = document.querySelector('.results');
-
-// buttonEl.addEventListener('click', () => {
-//   const allTeams = drivers.map(startRace);
-
-//   Promise.race(allTeams).then(({ teamName, time }) =>
-//     winnerEl.textContent = `${teamName} finished in ${time}`);
-
-//   winnerEl.textContent = '';
-// })
-
-
-
-// function startRace(teamName) {
-//   return new Promise((resolve) => {
-//     const time = Math.floor(Math.random() * (2000 - 1000 + 1) + 1000);
-
-//     setTimeout(() => {
-//       resolve({teamName, time});
-//     }, time)
-//   })
-// }
-
-
